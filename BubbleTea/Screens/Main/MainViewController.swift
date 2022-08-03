9d07c6f5bd5594a594b43e69c86f28b675ea836f
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-08-03 21:48:31 +0300

    Initial project

---
+++ BubbleTea/Screens/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  BubbleTea
+//
+//  Created by Stanislav Lemeshaev on 03.08.2022.
+//  Copyright © 2022 slemeshaev. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
