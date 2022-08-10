dbc5df075409ce65bf9bd378cceb4eeab7f33424
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-08-10 22:07:57 +0300

    Replaced ViewController with TableViewController

--- BubbleTea/Screens/Filter/FilterViewController.swift
+++ BubbleTea/Screens/Filter/FilterViewController.swift
@@ -8,25 +8,25 @@
 
 import UIKit
 
-class FilterViewController: UIViewController {
+class FilterViewController: UITableViewController {
     // MARK: - IBOutlets
-    @IBOutlet private weak var firstPriceCategoryLabel: UILabel!
-    @IBOutlet private weak var secondPriceCategoryLabel: UILabel!
-    @IBOutlet private weak var thirdPriceCategoryLabel: UILabel!
-    @IBOutlet private weak var numDealsLabel: UILabel!
+    @IBOutlet private weak var firstPriceCategory: UILabel!
+    @IBOutlet private weak var secondPriceCategory: UILabel!
+    @IBOutlet private weak var thirdPriceCategory: UILabel!
+    @IBOutlet private weak var numDeals: UILabel!
     
-    @IBOutlet private weak var cheapVenueCell: UITableViewCell!
-    @IBOutlet private weak var moderateVenueCell: UITableViewCell!
-    @IBOutlet private weak var expensiveVenueCell: UITableViewCell!
+    @IBOutlet private weak var cheapVenue: UITableViewCell!
+    @IBOutlet private weak var moderateVenue: UITableViewCell!
+    @IBOutlet private weak var expensiveVenue: UITableViewCell!
     
-    @IBOutlet private weak var offeringDealCell: UITableViewCell!
-    @IBOutlet private weak var walkingDistanceCell: UITableViewCell!
-    @IBOutlet private weak var userTipsCell: UITableViewCell!
+    @IBOutlet private weak var offeringDeal: UITableViewCell!
+    @IBOutlet private weak var walkingDistance: UITableViewCell!
+    @IBOutlet private weak var userTips: UITableViewCell!
     
-    @IBOutlet private weak var nameAZSortCell: UITableViewCell!
-    @IBOutlet private weak var nameZASortCell: UITableViewCell!
-    @IBOutlet private weak var distanceSortCell: UITableViewCell!
-    @IBOutlet private weak var priceSortCell: UITableViewCell!
+    @IBOutlet private weak var nameAZSort: UITableViewCell!
+    @IBOutlet private weak var nameZASort: UITableViewCell!
+    @IBOutlet private weak var distanceSort: UITableViewCell!
+    @IBOutlet private weak var priceSort: UITableViewCell!
     
     // MARK: - Life cycle
     override func viewDidLoad() {
@@ -39,7 +39,7 @@ class FilterViewController: UIViewController {
 }
 
 // MARK: - UITableViewDelegate
-extension FilterViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+extension FilterViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     }
 }
